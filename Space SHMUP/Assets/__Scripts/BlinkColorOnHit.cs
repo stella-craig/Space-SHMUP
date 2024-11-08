8a9581202cbca847c49489739167e630b66bf272
Stella <112723364+stella-craig@users.noreply.github.com>
2024-11-08 14:58:10 -0600

    Enemy_4 - A More Complex Enemy

--- Space SHMUP/Assets/__Scripts/BlinkColorOnHit.cs
+++ Space SHMUP/Assets/__Scripts/BlinkColorOnHit.cs
@@ -11,6 +11,7 @@ public class BlinkColorOnHit : MonoBehaviour
     [Header("Dynamic")]
     public bool showingColor = false;
     public float blinkCompleteTime; // Time to stop showing the color
+    public bool ignoreOnCollisionEnter = false;
 
     private Material[] materials; // All the Materials of this & its children
     private Color[] originalColors;
@@ -36,6 +37,8 @@ public class BlinkColorOnHit : MonoBehaviour
 
     private void OnCollisionEnter(Collision coll)
     {
+        if (ignoreOnCollisionEnter) return;
+
         // Check for collisions with ProjectileHero
         ProjectileHero p = coll.gameObject.GetComponent<ProjectileHero>();
         if(p!= null)
@@ -53,7 +56,7 @@ public class BlinkColorOnHit : MonoBehaviour
     ///     materials array to blinkColor, sets showingColor to true, and sets the
     ///     time that the colors should be reverted.
     /// </summary>
-    void SetColors()
+    public void SetColors()
     {
         foreach(Material m in materials)
         {
