cbb4b32c89a966c2f20f763de4534d4d650731b0
Stella <112723364+stella-craig@users.noreply.github.com>
2024-11-08 12:00:50 -0600

    Making Enemies Drop PowerUps

--- Space SHMUP/Assets/__Scripts/Enemy.cs
+++ Space SHMUP/Assets/__Scripts/Enemy.cs
@@ -11,7 +11,9 @@ public class Enemy : MonoBehaviour
     public float fireRate = 0.3f; // Seconds/shot (Unused)
     public float health = 10; // Damage needed to destroy this enemy
     public int score = 100; // Points earned for destroying this
+    public float powerUpDropChance = 1f; // Chance to drop a PowerUp
 
+    protected bool calledShipDestroyed = false;
     protected BoundsCheck bndCheck;
 
     void Awake()
@@ -89,6 +91,12 @@ public class Enemy : MonoBehaviour
                 health -= Main.GET_WEAPON_DEFINITION(p.type).damageOnHit;
                 if (health <= 0)
                 {
+                    // Tell Main that this ship was destroyed
+                    if (!calledShipDestroyed)
+                    {
+                        calledShipDestroyed = true;
+                        Main.SHIP_DESTROYED(this);
+                    }
                     // Destroy this Enemy
                     Destroy(this.gameObject);
                 }
